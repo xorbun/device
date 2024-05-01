7a055dd300d0a8cde686d51b759b46490c7d2710
Bruno Capobianco <bruno.capobianco@outlook.it>
2024-05-01 17:29:46 +0200

    Update ImagePicker.js
    takes photos and shows the taken picture

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,6 +1,9 @@
-import { Alert, Button, View } from "react-native"
+import { Alert, Button, Image, View,Text,StyleSheet } from "react-native"
 import {launchCameraAsync,useCameraPermissions,PermissionStatus} from 'expo-image-picker'
+import { useState } from "react"
+import { Colors } from "../../constants/color"
 const ImagePicker=()=>{
+    const [pickedImage,setPickedImage]=useState()
     const [cameraPermissionStatus,requestPermission]=useCameraPermissions()
     const verifyPermission=async()=>{
         if(cameraPermissionStatus.status===PermissionStatus.UNDETERMINED){
@@ -23,13 +26,38 @@ const ImagePicker=()=>{
         aspect:[16,9],
         quality:0.5
       })  
+      setPickedImage(image.assets[0].uri)
       console.log(image)
+    }
+    let imagePreview=<Text style={styles.text}>No image taken yet</Text>
+    if(pickedImage){
+        imagePreview=<Image style={styles.image} source={{uri:pickedImage}}/>
     }
     return(
         <View>
-            <View></View>
+            <View style={styles.imagePreview}> 
+                {imagePreview}
+            </View>
             <Button title='take image' onPress={takeImageHandler}/>
         </View>
     )
 }
 export default ImagePicker
+const styles=StyleSheet.create({
+    text:{
+        color:Colors.primary500
+    },
+    imagePreview:{
+        width:'100%',
+        height:200,
+        marginVertical:8,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:Colors.primary100,
+        borderRadius:4
+    },
+    image:{
+        width:'100%',
+        height:'100%'
+    }
+})
